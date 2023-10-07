e3dd5d5e772859589c96a1538327f091d1223049
Haopeng <haopeng138@gmail.com>
2023-10-07 19:23:07 +0200

    Homewor2

--- matlab/Session6.m
+++ matlab/Session6.m
@@ -38,3 +38,51 @@ figure,imshow(imGaus,[]),title('Filtrat gausià')
 figure,imshow(im-imGaus,[]),title('Filtrat gausià')
 
 
+%% Homework
+
+
+im = double(imread('rabbit.jpg'));
+imshow(im,[])
+%% Aplicar Laplaciano
+hL = [0 1 0;1 -4 1;0 1 0];
+imLaplacia = imfilter(im,hL);
+imshow(imLaplacia,[])
+
+%% Laplaciano + Gaussiano
+w = fspecial('log',5);
+imLoG=imfilter(im,w);
+figure,imshow(imLoG,[]),title('LoG')
+
+
+%% Detección de pasos por zero
+ 
+% Busca la laplaciana que pasa por zero
+% Buscar aquellos pixeles en que tenga un vecino que sea positivo y alguno
+% que sea negativo
+
+valorPos = imLoG > 0; % pixeles con valor positivo
+valorNeg = imLoG < 0; % pixeles con valor negativo
+
+
+sumPos = colfilt(valorPos,[3 3],"sliding",@sum); % obtiene el número de vecinos positivos
+sumNeg = colfilt(valorNeg,[3 3],"sliding",@sum); % obtiene el número de vecinos negativos
+% Nota: la función colfilt nos proporciona los vecinos de cada pixel y
+% después aplica la función que le pasamos con los vecinos de cada pixel
+
+UP = sumPos > 0; % Al menos tiene un vecino positvo
+UN = sumNeg > 0; % Al menos tiene un vecino negativo
+
+Z = UP & UN; % Los que tiene al menos un vecino positivo y negativo, entonces pasa por zero
+figure,imshow(Z),title("Paso por zero")
+
+%% Utilizar el gradiente de clase
+Gx = imfilter(im, dog);
+Gy = imfilter(im, dog'); 
+G = abs(Gx) + abs(Gy); % modul del gradiente (dx,dy)
+
+
+
+R = Z .* G; % Resaltar los pixeles de paso por zero con el modulo del gradiente
+R(R>5)= 255; % Limpiar un poco de ruido en la imagen
+figure,imshow(R,[]),title("Imagen de contorno");
+
